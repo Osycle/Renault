c751f216260064918da53b3d8ecca0a524fd6598
Osycle <osycle@gmail.com>
2021-09-21 17:48:54 +0500

    20210921

---
+++ app/js/main.js
@@ -0,0 +1,182 @@
+"use strict";
+
+
+
+(function() {
+	$(function() {
+		//$("img").attr("src", "img/other/short-news-5.jpg");
+		//$("[style*='background-image']").attr("style", "background-image: url('img/other/short-news-5.jpg')");
+
+		/*AOS*/
+		if( "AOS" in window ){
+			AOS.init({
+				offset: 100,
+				once: true,
+				duration: 1100,
+				delay: 150
+			});
+			setTimeout(function() { AOS.refresh(); }, 1);
+		}
+
+		/* bootstrap tooltip*/		
+		$('[data-toggle="tooltip"]').tooltip({
+			html: true
+		})
+		/* SELECT2 */
+		if ( $(".js-select").length )
+			$(".js-select").select2({
+				placeholder: "Выберите...",
+				minimumResultsForSearch: Infinity,
+				allowClear: false
+			});
+		
+
+
+
+
+		$('[data-toggle="click"]').on("click", function(e){
+			var that = $(this);
+			e.preventDefault();
+			var activeClass = that.attr("toggle-class") || "active";
+			that.toggleClass(activeClass);
+			$(that.attr("href")).toggleClass(activeClass);
+		})
+
+		$('.sidebar-wrapper').theiaStickySidebar({
+      additionalMarginTop: 30
+    });
+
+		$(".menu-items a").on("click", function(){
+			location.href = $(this).attr("href");
+			console.log(location.hash, $(this).attr("href"))
+			if( location.pathname+location.hash == $(this).attr("href") )
+				location.reload();
+		})
+    
+
+		/*FANCYBOX*/
+		if ($("[data-fancybox]").length != 0)
+			$("[data-fancybox]").fancybox({
+				afterShow: function(instance, current) {},
+				animationEffect : "zoom",
+				animationDuration : 800,
+				toolbar : true,
+				keyboard : true,
+				thumbs : {
+					autoStart   : false
+				},
+				touch : false,
+				transitionDuration : 366,
+				transitionEffect: "zoom-in-out"
+			});
+
+
+
+		//MIN-MENU
+		$("#min-menu").mmenu({
+			extensions: [
+				"wrapper-bg", // wrapper-bg black
+				//"theme-dark",
+				"theme-white",
+				//"fullscreen",
+				"listview-50",
+				"fx-panels-slide-up",
+				"fx-listitems-drop",
+				"border-offset",
+				"position-front",
+				"position-right"
+			],
+			navbar: {
+				title: ""
+			},
+			navbars: [{
+					height: 0,
+					content: [
+						// '<div class="close-btn close-content bar">' +
+						// '<a  href="#page" ><span class="icon-bar"></span><span class="icon-bar"></span></a>' +
+						// '</div>'
+					]
+				},
+				{
+					content: ["prev", "title"]
+				}
+			]
+		}, {});
+
+
+		// SMOTHSCROLL-LINK
+		window.smoothScroll = new SmoothScroll('[scroll-link]', {
+			speed: 500,
+			speedAsDuration: true
+		});
+
+
+
+	});
+})(jQuery);
+
+var isWebkit = /Webkit/i.test(navigator.userAgent),
+		isChrome = /Chrome/i.test(navigator.userAgent),
+		isMac = /Mac/i.test(navigator.userAgent),
+		isMobile = !!("ontouchstart" in window),
+		isAndroid = /Android/i.test(navigator.userAgent),
+		isEdge = /Edge/i.test(navigator.userAgent);
+
+
+// COMMON FUNCTION
+
+
+
+
+function checkSm() {
+	return $(document).width() <= 991;
+}
+
+function checkMd() {
+	return $(document).width() < 1199 && !checkSm();
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntFloat(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function onResized(f) {
+	if (typeof f === "function") f();
+	$(window).on("resize", function(e) {
+		if (typeof f === "function") f();
+	});
+	return this;
+}
+
+function scrolledDiv(el) {
+	try {
+		var docViewTop = $(window).scrollTop(),
+			docViewBottom = docViewTop + $(window).height(),
+			elTop = $(el).offset().top + 100,
+			elBottom = elTop + $(el).height() / 1.8;
+	} catch (err) {
+		console.error();
+	}
+
+	return elBottom <= docViewBottom && elTop >= docViewTop;
+}
+
+function roundFix( num, cnt ){
+	num = num+""
+	cnt = cnt + (/./.test(num) || null ? 1 : 0);
+	return num.substring( 0,  cnt)*1
+}
+
+
+function spaceBetweenNum(str, char) {
+	str = str+"";
+	char = char || ","
+	var pattern = /(-?\d+)(\d{3})/;
+	while (pattern.test(str))
+		str = str.replace(pattern, "$1"+char+"$2");
+	return str;
+}
---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+
+
+
+var gulp 					= require('gulp'),
+		browserSync 		= require('browser-sync'),
+		sass 						= require('gulp-sass'),
+		concat					= require('gulp-concat'),
+		uglify					= require('gulp-uglifyjs'),
+		cssnano					= require('gulp-cssnano'),
+		rename					= require('gulp-rename'),
+		autoprefixer		= require('gulp-autoprefixer');
+		
+var app = "app";
+
+
+
+
+
+// SASS
+gulp.task('sass', (done)=>{
+		gulp
+			.src( app+'/scss/**/*.+(scss|sass)')
+			.pipe( sass().on('error', sass.logError) )
+			.pipe(autoprefixer({overrideBrowserslist: 'last 2 versions', cascade: false} ))
+			.pipe( gulp.dest(app+'/css/') )
+			.pipe( browserSync.reload({stream:true}) );
+		done();
+	}
+);
+
+
+// SCRIPTS
+gulp.task('scripts', ()=>{
+	return gulp.src([
+			app+'/js/plugins/jquery.min.js',
+			//app+'/js/plugins/jquery-ui.js',
+			//app+'/js/plugins/skrollr.min.js',
+			//app+'/js/plugins/smoothscroll.js',
+			app+'/js/plugins/bootstrap.min.js',
+			//app+'/js/plugins/howler.js', 
+			//app+'/js/plugins/TweenMax.min.js',
+			//app+'/js/plugins/EasePack.min.js', 
+			//app+'/js/plugins/TextPlugin.min.js',
+			//app+'/js/plugins/konva.min.js',
+			//app+'/js/plugins/KonvaPlugin.js',
+			//app+'/js/plugins/jquery.fractionslider.js',
+			//app+'/js/plugins/pana-accordion.js',
+			//app+'/js/plugins/jquery.vi.js',
+			app+'/js/plugins/aos.js',
+			//app+'/js/plugins/wow.js',
+			app+'/js/plugins/owl.carousel.min.js',
+			app+'/js/plugins/jquery.fancybox.js',
+			//app+'/js/plugins/jquery.jcarousel.js',
+			//app+'/js/plugins/classie.js',
+			//app+'/js/plugins/masonry.pkgd.min.js',
+			app+'/js/plugins/select2.min.js',
+			//app+'/js/plugins/jquery.elevateZoom.min.js',
+			app+'/js/plugins/jquery.mmenu.all.js',
+			app+'/js/plugins/smooth-scroll.js',
+			app+'/js/plugins/ion.rangeSlider.min.js',
+			app+'/js/plugins/ResizeSensor.min.js',
+			app+'/js/plugins/theia-sticky-sidebar.min.js',
+			//app+'/js/plugins/parallax-mouse.js',
+			//app+'/js/plugins/parallax.js',
+			app+'/js/plugins/flickity.js'
+		])
+		.pipe( concat('scripts.min.js') )
+		.pipe( uglify() )
+		.pipe( gulp.dest(app+'/js/') ); //js default
+	}
+);
+
+// STYLES
+gulp.task('cssnano', ()=>{
+		return gulp.src(app+'/css/main.css')
+		.pipe( cssnano({ reduceIdents :  false }) )
+		.pipe(rename({suffix: '.min'}) )
+		.pipe( gulp.dest(app+'/css/') ); //css default
+	}
+);
+
+// RELOADER BROWSER
+gulp.task('browser-sync', (done)=>{
+		browserSync({
+			server: {baseDir: app+''},
+			//proxy: "http://taf/",
+			notify: false
+		});
+		done();
+	}
+);
+gulp.task('watch', (done)=>{
+		//gulp.task('default', gulp.series('watch', 'sass', 'browser-sync'));
+		gulp.watch(app+'/scss/**/*.+(scss|sass)', gulp.parallel('sass'));
+		gulp.watch(app+'/*.html').on("change", browserSync.reload);
+		gulp.watch(app+'/**/*.php', browserSync.reload);
+		gulp.watch(app+'/templates/**/*.tpl', browserSync.reload);
+		gulp.watch(app+'/js/**/*.js', browserSync.reload);
+		done();
+	}
+);
+
+
+//gulp.task('default', gulp.series('watch', 'sass'));
+gulp.task('watch', gulp.series('watch','browser-sync', 'scripts'));
+
